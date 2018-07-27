4869d57affd9636aced81e4a34ded689f33c5581
Matthieu <matthieunadeau96@gmail.com>
2018-07-27 13:27:12 -0400

    Style Changes: Trying to get the Ranking and Record to fit nicely

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -11,10 +11,14 @@ class Stats extends Component {
         </div>
 
         <div className="CompetitiveInfo">
-          {this.props.rank && <h3>{this.props.rank}</h3>}
-          {this.props.rankIcon && <img className="rankIcon" src={this.props.rankIcon} alt="rank icon"/>}
-          {this.props.gamesWon && <p>Record: {this.props.gamesWon} - {this.props.gamesLost} - {this.props.gamesTied}</p>}
-          {this.props.timePlayed && <p>Time Played: {this.props.timePlayed}</p>}
+          <div className="ranking">
+            {this.props.rank && <p className="rankNumber">{this.props.rank}</p>}
+            {this.props.rankIcon && <img className="rankIcon" src={this.props.rankIcon} alt="rank icon"/>}
+          </div>
+          <div className="compRecord">
+            {this.props.gamesWon && <p>Record: {this.props.gamesWon} - {this.props.gamesLost} - {this.props.gamesTied}</p>}
+            {this.props.timePlayed && <p>Time Played: {this.props.timePlayed}</p>}
+          </div>
         </div>
 
         {this.props.error && <p>{this.props.error}</p>}
